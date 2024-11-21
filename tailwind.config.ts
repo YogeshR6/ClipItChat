3632d1b71ab82d89b02bf520b887394496134334
Yogesh Limbani <yrlimbani03@gmail.com>
2024-11-21 22:22:00 +0530

    Project reset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,18 @@
 import type { Config } from "tailwindcss";
-const flattenColorPalette =
-  require("tailwindcss/lib/util/flattenColorPalette").default;
 
-const config: Config = {
-    darkMode: ["class"],
-    content: [
+export default {
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate"), addVariablesForColors],
-};
-
-function addVariablesForColors({ addBase, theme }: any) {
-	let allColors = flattenColorPalette(theme("colors"));
-	let newVars = Object.fromEntries(
-	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-	);
-   
-	addBase({
-	  ":root": newVars,
-	});
-  }
-
-export default config;
+  plugins: [],
+} satisfies Config;
