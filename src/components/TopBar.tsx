414e63d7678844570c0ba18c1f73a3310d3e25ab
Yogesh Limbani <yrlimbani03@gmail.com>
2024-12-03 23:00:10 +0530

    Add profile image and logout dropdown menu

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import { useAuth } from "@/hooks/contexts/AuthContext";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
+import { Button } from "./ui/button";
+import { userSignOut } from "@/utils/authFunctions";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const TopBar: React.FC = () => {
+  const { user, isLoggedIn } = useAuth();
+  const handleUserLogout = async () => {
+    try {
+      await userSignOut();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-row justify-between w-full items-center fixed top-0 z-10 p-3">
       <div>Logo Here</div>
@@ -19,12 +40,35 @@ const TopBar: React.FC = () => {
         </Link>
       </div>
       <div>
-        <Link href="/auth">
-        <CgProfile style={{fontSize: 25}}/>
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src={user?.photoURL} alt="profile image" />
+                  <AvatarFallback>
+                    <CgProfile style={{ fontSize: 25 }} />
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem onClick={handleUserLogout}>
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        ) : (
+          <Link href="/auth">
+            <Button variant="outline">Login / Signup</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default TopBar;
